c21aeee27f8a0c4c91afed947e5ea32ae7720fe2
Anna Galstyan <43260520+annagalstyan14@users.noreply.github.com>
2025-09-14 12:06:57 +0400

    Update dp.h

--- Fibonacci/dp.h
+++ Fibonacci/dp.h
@@ -2,12 +2,11 @@
 #include <vector>
 
 template<typename T>
-T dp(T n){
-    if (n<=1) return n;
-    std::vector<T> dp(n+1);
-    dp[0] = 0; dp[1] = 1;
-    for (T i = 2, i <=n; i++)
-        dp[i] = dp[i-1] + dp[i-2];
-    return dp[n];
-    
+T prev2 = 0, prev1 = 1;
+    for (T i = 2; i <= n; i++) {
+        T curr = prev1 + prev2;
+        prev2 = prev1;
+        prev1 = curr;
+    }
+    return prev1;
 }
